62dd01096458390e5f89a26579bde6d75fc79075
unknown <crismai.dev@gmail.com>
2023-05-03 14:22:32 -0300

    Emitindo um evento para vários clientes - socket.broadcast

--- src/socket-back.js
+++ src/socket-back.js
@@ -5,6 +5,8 @@ io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
 
     socket.on("texto_editor", (texto) => {
-        console.log(texto);
+        socket.broadcast.emit("texto_editor_clientes", texto);
     });
+
+
 });
