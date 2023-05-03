07bf0ee88a6c017fcb1f664036cc795b974b6691
unknown <crismai.dev@gmail.com>
2023-05-03 14:12:46 -0300

    Emitindo eventos - socket.emit()

--- public/documento.js
+++ public/documento.js
@@ -1,2 +1,8 @@
 
 const socket = io();
+
+const textoEditor = document.getElementById("editor-texto");
+
+textoEditor.addEventListener("keyup", () => {
+    socket.emit("texto_editor", textoEditor.value);
+});
