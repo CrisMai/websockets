ceabd5ad89636dd6f57a431829022560b69b62a3
unknown <crismai.dev@gmail.com>
2023-05-08 14:14:34 -0300

    Consultando dados do banco

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,33 +1,19 @@
+import { documentosColecao } from "./dbConnect.js";
 import io from "./servidor.js";
 
-const documentos = [
-    {
-        nome: "JavaScript",
-        texto: "texto de javascript...",
-    },
-    {
-        nome: "Node",
-        texto: "texto de node...",
-    },
-    {
-        nome: "Socket.io",
-        texto: "texto de socket.io...",
-    }
-]
+
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
 
-    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+    socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento);
 
-        const documento = encontrarDocumento(nomeDocumento);
+        const documento = await encontrarDocumento(nomeDocumento);
 
         if(documento) {
-            
             devolverTexto(documento.texto);
-        }
-        
+        }     
     });
 
     socket.on("texto_editor", ({texto, nomeDocumento}) => {
@@ -41,8 +27,8 @@ io.on("connection", (socket) => {
 });
 
 function encontrarDocumento(nome) {
-    const documento = documentos.find((documento) => {
-        return documento.nome === nome;
+    const documento = documentosColecao.findOne({
+        nome
     });
 
     return documento;
