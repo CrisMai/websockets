d26e313325d3fd5f790b0172acec08114f02918c
unknown <crismai.dev@gmail.com>
2023-05-09 14:45:13 -0300

    Tratativa de erro

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -10,11 +10,14 @@ socket.emit("obter_documentos", (documentos) => {
 
 function emitirAdicionarDocumento(nome) {
     socket.emit("adicionar_documento", nome);
-}
+};
 
 socket.on("adicionar_documento_interface", (nome) => {
     inserirLinkDocumento(nome);
-})
+});
 
+socket.on("documento_existente", (nome) => {
+    alert(`O documento ${nome} já existe!`)
+})
 
 export {emitirAdicionarDocumento};
