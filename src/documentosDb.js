a915448a29ad02a92ab77c87a159fda4f0e30cfc
unknown <crismai.dev@gmail.com>
2023-05-09 14:29:37 -0300

    Adicionando um documento

--- src/documentosDb.js
+++ src/documentosDb.js
@@ -6,6 +6,14 @@ function obterDocumentos() {
     return documentos;
 }
 
+function adicionarDocumento(nome) {
+    const resultado = documentosColecao.insertOne({
+        nome,
+        texto:"",
+    });
+    return resultado;
+}
+
 function encontrarDocumento(nome) {
     const documento = documentosColecao.findOne({
         nome
@@ -27,4 +35,4 @@ function atualizaDocumento(nome, texto) {
 }
 
 
-export {encontrarDocumento, atualizaDocumento, obterDocumentos};
+export {encontrarDocumento, atualizaDocumento, obterDocumentos, adicionarDocumento};
