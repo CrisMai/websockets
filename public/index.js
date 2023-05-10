695466ff587b8ac56acfc3dbf2781da8ad8baad5
unknown <crismai.dev@gmail.com>
2023-05-10 16:04:39 -0300

    Refatorando o projeto

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 import { emitirAdicionarDocumento } from "./socket-front-index.js";
 
 const listaDocumentos = document.getElementById("lista-documentos");
@@ -6,26 +5,27 @@ const form = document.getElementById("form-adiciona-documento");
 const inputDocumento = document.getElementById("input-documento");
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault();
-    emitirAdicionarDocumento(inputDocumento.value);
-    inputDocumento.value = "";
+  evento.preventDefault();
+  emitirAdicionarDocumento(inputDocumento.value);
+  inputDocumento.value = "";
 });
 
 function inserirLinkDocumento(nomeDocumento) {
-    listaDocumentos.innerHTML += `
-        <a 
-            href="documento.html?nome=${nomeDocumento}" 
-            class="list-group-item list-group-item-action"
-            id="documento-${nomeDocumento}"
-        >
-            ${nomeDocumento}
-        </a>
-    `;
+  listaDocumentos.innerHTML += `
+    <a
+      href="/documento/index.html?nome=${nomeDocumento}"
+      class="list-group-item list-group-item-action"
+      id="documento-${nomeDocumento}"
+    >
+      ${nomeDocumento}
+    </a>
+  `;
 }
 
 function removerLinkDocumento(nomeDocumento) {
-    const documento = document.getElementById(`documento-${nomeDocumento}`);
-    listaDocumento.removeChild(documento);
+  const documento = document.getElementById(`documento-${nomeDocumento}`);
+
+  listaDocumentos.removeChild(documento);
 }
 
-export {inserirLinkDocumento, removerLinkDocumento};
+export { inserirLinkDocumento, removerLinkDocumento };
