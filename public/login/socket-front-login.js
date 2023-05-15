e335db2bb0d72687754e1c6f05dc189306f380b9
unknown <crismai.dev@gmail.com>
2023-05-15 14:08:50 -0300

    Enviando dados de login

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,9 @@
+
+const socket = io();
+
+function emitirAutenticarUsuario(dados) {
+    socket.emit("autenticar_usuario", dados);
+}
+
+
+export {emitirAutenticarUsuario};
--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -2,6 +2,7 @@
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
@@ -9,4 +10,5 @@ io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
